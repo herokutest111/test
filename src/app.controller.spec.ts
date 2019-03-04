19961e8768930e4c188a0a4a032f2d5e28f21527
herokutest111 <47941612+herokutest111@users.noreply.github.com>
2019-03-05 01:04:48 +0200

    Feature/name
    * change name
    
    * feature/name2

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello WWWWWWgfffhgfjhg!"', () => {
       expect(appController.getHello()).toBe('Hello WWWWWW!');
     });
   });
